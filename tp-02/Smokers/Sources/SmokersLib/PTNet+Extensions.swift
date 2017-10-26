0fb041a4306f3a8d26cabe10e3349229a986ba15
GregoryBays <gregory.bays@etu.unige.ch>
2017-10-26 15:57:34 +0200

    tp216h

--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -15,8 +15,45 @@ public class MarkingGraph {
 public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
+
         // Write here the implementation of the marking graph generation.
-        return nil
+
+        let m0:MarkingGraph = MarkingGraph(marking: marking)
+
+        var NodeVisited:[MarkingGraph] = [m0]
+
+        var ToVisit:[MarkingGraph] = [m0]
+
+        while let actu = ToVisit.popLast(){
+
+          NodeVisited.append(actu)
+
+          for tran in transitions{
+
+            if let firedTran = tran.fire(from: actu.marking){
+
+              if let visited = NodeVisited.first(where: {$0.marking == firedTran}){
+
+                actu.successors[tran] = visited
+
+              } else{
+
+                let temp:MarkingGraph = MarkingGraph(marking: firedTran)
+
+                if !NodeVisited.contains{$0 === temp}{
+
+                  actu.successors[tran] = temp
+
+                  ToVisit.append(temp)
+                }
+              }
+            }
+          }
+        }
+
+        print("\(NodeVisited)")
+
+        return m0
     }
 
 }
