5284b6f65828c1f0c567d846b03e4dfec66b1555
GregoryBays <gregory.bays@etu.unige.ch>
2017-11-09 13:55:54 +0100

    tp3

--- tp-03/Coverability/Sources/Coverability/main.swift
+++ tp-03/Coverability/Sources/Coverability/main.swift
@@ -13,3 +13,53 @@ import CoverabilityLib
 // You **are** encouraged to write tests of your own!
 // You may write as many tests as you want here, or even better in `CoverabilityLibTests.swift`.
 // Observe how the two existing tests are implemented to write your own.
+
+
+print("TP03 - Graphe de Couverture\n")
+
+//modèle 1
+
+print("Essai avec le createBoundedModel")
+
+do{
+  let model = createBoundedModel()
+
+  guard let r = model.places.first(where: { $0.name == "r" }),
+        let s1 = model.places.first(where: { $0.name == "s1" }),
+        let s2 = model.places.first(where: { $0.name == "s2" }),
+        let s3 = model.places.first(where: { $0.name == "s3" }),
+        let p = model.places.first(where: { $0.name == "p" }),
+        let t  = model.places.first(where: { $0.name == "t"  }),
+        let m  = model.places.first(where: { $0.name == "m"  }),
+        let w1  = model.places.first(where: { $0.name == "w1"  }),
+        let w2  = model.places.first(where: { $0.name == "w2"  }),
+        let w3  = model.places.first(where: { $0.name == "w3"  })
+        else {
+          fatalError("il y a une erreur avec le modèle")
+        }
+
+  let initialMarking: CoverabilityMarking =
+      [r: 1, p: 1, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+}
+
+//modèle 2
+
+print("Test avec le createUnboundedModel (Peut prendre un petit moment)")
+do{
+  let model = createUnboundedModel()
+
+  guard let s0 = model.places.first(where: { $0.name == "s0" }),
+        let s1 = model.places.first(where: { $0.name == "s1" }),
+        let s2 = model.places.first(where: { $0.name == "s2" }),
+        let s3 = model.places.first(where: { $0.name == "s3" }),
+        let s4 = model.places.first(where: { $0.name == "s4" }),
+        let b  = model.places.first(where: { $0.name == "b"  })
+
+        else {
+          fatalError("il y a une erreur avec le modèle")
+        }
+
+  let initialMarking: CoverabilityMarking =
+      [s0: 1, s1: 0, s2: 1, s3: 0, s4: 1, b: 1]
+
+}
--- tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
+++ tp-03/Coverability/Sources/CoverabilityLib/PTNet+Extensions.swift
@@ -1,19 +1,125 @@
 import PetriKit
 
 public extension PTNet {
+    
+    public func coverabilityToPTMarking(with marking : CoverabilityMarking, and p : [PTPlace]) -> PTMarking{
+      var m : PTMarking = [:]
 
-    public func coverabilityGraph(from marking: CoverabilityMarking) -> CoverabilityGraph {
-        // Write here the implementation of the coverability graph generation.
-
-        // Note that CoverabilityMarking implements both `==` and `>` operators, meaning that you
-        // may write `M > N` (with M and N instances of CoverabilityMarking) to check whether `M`
-        // is a greater marking than `N`.
-
-        // IMPORTANT: Your function MUST return a valid instance of CoverabilityGraph! The optional
-        // print debug information you'll write in that function will NOT be taken into account to
-        // evaluate your homework.
-
-        return CoverabilityGraph(marking: marking)
+      for temp in p
+      {
+        let this = correctValue(to : marking[temp]!)!
+        m[temp] = this
+      }
+      return m
     }
 
+
+    public func ptmarkingToCoverability(with marking: PTMarking, and p : [PTPlace]) ->CoverabilityMarking{
+      var temp : CoverabilityMarking = [:]
+      for val in p
+      {
+        temp[val] = .some(marking[val]!)
+        if(500 < temp[val]!)
+        {
+          temp[val] = .omega
+        }
+      }
+      return temp
+    }
+
+
+    public func correctValue(to t: Token) -> UInt? {
+      if case .some(let value) = t {
+        return value
+      }
+      else {
+        return 1000
+      }
+    }
+
+    //véfifie les noeuds
+
+    public func verify(at marking : [CoverabilityMarking], to markingToAdd : CoverabilityMarking) -> Int
+    {
+      var value = 0
+      for i in 0...marking.count-1
+
+      {
+        if (marking[i] == markingToAdd)
+
+        {
+          value = 1
+        }
+        if (markingToAdd > marking[i])
+
+        {
+          value = i+2}
+      }
+      return value
+    }
+
+    // ajoute omega
+
+    public func Omega(from comp : CoverabilityMarking, with marking : CoverabilityMarking, and p : [PTPlace])  -> CoverabilityMarking?
+    {
+      var temp = marking
+      for t in p
+      {
+        if (comp[t]! < temp[t]!)
+        {
+          temp[t] = .omega
+        }
+      }
+      return temp
+    }
+
+    public func coverabilityGraph(from marking0: CoverabilityMarking) -> CoverabilityGraph? {
+
+        var transitionsC = Array (transitions) // sort les valeurs de l'array
+        transitionsC.sort{$0.name < $1.name}
+        let placesC = Array(places)
+
+        var markingList : [CoverabilityMarking] = [marking0]
+        var graphList : [CoverabilityGraph] = []
+        var this: CoverabilityMarking
+        let returnedGraph = CoverabilityGraph(marking: marking0, successors: [:])
+        var count = 0
+
+        while(count < markingList.count)
+        {
+        for tran in transitionsC
+        {
+            let ptMarking = coverabilityToPTMarking(with: markingList[count], and: placesC)
+            if let firedTran = tran.fire(from: ptMarking){
+
+              let convMarking = ptmarkingToCoverability(with: firedTran, and: placesC)
+
+              let newCouv = CoverabilityGraph(marking: convMarking, successors: [:])
+
+              returnedGraph.successors[tran] = newCouv
+            }
+
+            if(returnedGraph.successors[tran] != nil){
+
+              this = returnedGraph.successors[tran]!.marking
+
+              let cur = verify(at: markingList, to: this)
+
+              if (cur != 1)
+              {
+                if (cur > 1)
+                {
+                  this = Omega(from : markingList[cur-2], with : this, and : placesC)!
+                }
+
+                graphList.append(returnedGraph)
+
+                markingList.append(this)
+              }
+            }
+          }
+          count = count + 1
+        }
+        return returnedGraph
+      }
 }
