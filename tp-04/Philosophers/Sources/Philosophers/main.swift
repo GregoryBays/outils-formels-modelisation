f6665e9390564a31e4be18ac9c908f0cdc9bb3b2
GregoryBays <gregory.bays@etu.unige.ch>
2017-11-23 23:36:27 +0100

    update main

--- tp-04/Philosophers/Sources/Philosophers/main.swift
+++ tp-04/Philosophers/Sources/Philosophers/main.swift
@@ -50,3 +50,29 @@ do {
         print(m)
     }
 }
+
+
+//Q1
+do{
+  let philosophers = lockFreePhilosophers(n: 5)
+  let graph = philosophers.markingGraph(from: philosophers.initialMarking!)
+  print("Marquage non bloquable : \(graph!.count)")
+}
+
+
+//Q2
+do{
+  let philosophers = lockablePhilosophers(n: 5)
+  let graph = philosophers.markingGraph(from: philosophers.initialMarking!)
+  print("Marquage bloquable \(graph!.count)")
+}
+
+
+//Q3 
+do {
+  let philosophers = lockablePhilosophers(n: 5)
+  let graph = philosophers.markingGraph(from: philosophers.initialMarking!)
+  for g in graph! {
+    print("\(g.marking)")
+  }
+}
